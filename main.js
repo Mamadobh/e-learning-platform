6d45960302942e345dae9d557615fd6d769f6f47
Mamadobh <mohamedbenhafsia2001@gmail.com>
2024-05-12 00:56:46 +0100

    work'

---
+++ main.js
@@ -0,0 +1,42 @@
+// let checkboxElement = document.querySelector("#icn");
+// let sidebar_element = document.createElement("div");
+// let gridElement = document.createElement("div");
+// gridElement.className="gridE"
+// let allContent = document.createElement("div");
+// let infoElement = document.querySelector(".information")
+// let landElement= document.getElementById("land")
+// let introElement = document.querySelector(".intro")
+
+// allContent.className="all"
+//  sidebar_element.className="sidebar";
+//  sidebar_element.innerHTML=	' <header>Summary</header><a href="#"><i class="fa-solid fa-angles-right"></i><span class="act">Link1</span></a>';
+//  sidebar_element.innerHTML= sidebar_element.innerHTML+'<a href="#"><i class="fa-solid fa-angles-right"></i><span class="act">Link2</span></a>';
+//  sidebar_element.innerHTML= sidebar_element.innerHTML+'<a href="#"><i class="fa-solid fa-angles-right"></i><span>Link3</span></a>';
+//  sidebar_element.innerHTML= sidebar_element.innerHTML+'	<a href="#"><i class="fa-solid fa-angles-right"></i><span>Link3</span></a>';
+//  sidebar_element.innerHTML= sidebar_element.innerHTML+'<a href="#"><i class="fa-solid fa-angles-right"></i><span>Link4</span></a>';
+//  sidebar_element.innerHTML= sidebar_element.innerHTML+'<a href="#"><i class="fa-solid fa-angles-right"></i><span>Link5</span></a>';
+//  sidebar_element.innerHTML= sidebar_element.innerHTML+'<a href="#"><i class="fa-solid fa-angles-right"></i><span>Link6</span></a>';
+
+// checkboxElement.addEventListener("click",(e)=> {
+// if(checkboxElement.checked === true){
+
+// gridElement.appendChild(sidebar_element);
+// allContent.appendChild(landElement);
+// allContent.appendChild(infoElement)
+// allContent.appendChild(introElement)
+// gridElement.appendChild(allContent);
+// console.log(gridElement);
+// document.body.appendChild(gridElement);
+// console.log(document.body.innerHTML)
+// // document.body.removeChild(landElement)    
+// // document.body.removeChild(introElement)    
+// // document.body.removeChild(infoElement)    
+
+// }
+// else{
+//     gridElement.removeChild(sidebar_element);
+//     let content = allContent.innerHTML;
+//      gridElement.remove();
+//     body.appendChild(content) 
+// }
+// })
